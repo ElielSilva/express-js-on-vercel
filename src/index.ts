6a77bb4ae7b73ea32c86180a0664128dea01ac4e
ElielSilva <silvaeliel940@gmail.com>
2025-12-09 12:26:24 -0300

    foi11

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url';
 // import { characters } from './database/character.js'
-import villages from "./database/villages.js";
+// import villages from "./database/villages.js";
 
-console.log(villages);
+// console.log(villages);
 
 const characters = [
   {
@@ -1279,6 +1279,73 @@ const characters = [
     "clan": ["Hyūga"]
   }
 ]
+
+const villages = [
+  {
+    id: 1,
+    name: "Vila Oculta da Folha (Konohagakure)",
+    region: "País do Fogo",
+    leader: "Hokage",
+    population: 1500,
+    clans: ["Uchiha", "Hyuga", "Akimichi", "Nara", "Yamanaka", "Sarutobi"]
+  },
+  {
+    id: 2,
+    name: "Vila Oculta da Areia (Sunagakure)",
+    region: "País do Vento",
+    leader: "Kazekage",
+    population: 1200,
+    clans: ["Clã Kazekage", "Clã Chiyo"]
+  },
+  {
+    id: 3,
+    name: "Vila Oculta da Névoa (Kirigakure)",
+    region: "País da Água",
+    leader: "Mizukage",
+    population: 1000,
+    clans: ["Hozuki", "Kaguya", "Yuki"]
+  },
+  {
+    id: 4,
+    name: "Vila Oculta da Pedra (Iwagakure)",
+    region: "País da Terra",
+    leader: "Tsuchikage",
+    population: 1100,
+    clans: ["Kamizuru", "Akatsuchi"]
+  },
+  {
+    id: 5,
+    name: "Vila Oculta da Nuvem (Kumogakure)",
+    region: "País do Raio",
+    leader: "Raikage",
+    population: 1300,
+    clans: ["Yotsuki", "Clã do Killer Bee"]
+  },
+  {
+    id: 6,
+    name: "Vila Oculta da Chuva (Amegakure)",
+    region: "País da Chuva",
+    leader: "Pain",
+    population: 800,
+    clans: ["Clãs Civis de Amegakure"]
+  },
+  {
+    id: 7,
+    name: "Vila Oculta da Cachoeira (Takigakure)",
+    region: "País da Cachoeira",
+    leader: "Shibuki",
+    population: 600,
+    clans: ["Clã de Takigakure"]
+  },
+  {
+    id: 8,
+    name: "Vila Oculta do Som (Otogakure)",
+    region: "País do Som",
+    leader: "Orochimaru",
+    population: 700,
+    clans: ["Quarteto do Som", "Clã Dosu"]
+  }
+];
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
